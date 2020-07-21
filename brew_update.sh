fb18b0f21d8bdeda424349a0a26d714a6613479b
kawarimidoll <kawarimidoll+git@gmail.com>
2020-07-21 10:18:46 +0900

    refactor brew_update.sh

--- brew_update.sh
+++ brew_update.sh
@@ -6,12 +6,11 @@ brew update
 brew upgrade
 brew cask upgrade
 brew cleanup
-date
 set +x
 echo 'log list...'
 LOGFILE=~/Desktop/brew-list.log
 echo '/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"' > $LOGFILE
-date >> $LOGFILE
+date "+%F %T %Z" | tee -a $LOGFILE
 brew list >> $LOGFILE
 brew cask list >> $LOGFILE
 cp $LOGFILE ./
