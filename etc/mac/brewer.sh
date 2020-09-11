e42ec5b56bcf04ca1d4028baf3c7295f05a213de
kawarimidoll <kawarimidoll+git@gmail.com>
2020-09-11 09:14:13 +0900

    update brewer

--- etc/mac/brewer.sh
+++ etc/mac/brewer.sh
@@ -5,14 +5,14 @@ set -x
 brew doctor || exit 1
 brew update
 brew upgrade
-brew cask upgrade
+brew upgrade --cask
 brew cleanup
 set +x
 echo 'log list...'
 logfile="${DOT_DIR}/etc/mac/brew-list.log"
 date "+timestamp: %F %T %Z" > $logfile
-brew tap       | sed 's/^/tap: /'  >> $logfile
-brew leaves    | sed 's/^/brew: /' >> $logfile
-brew cask list | sed 's/^/cask: /' >> $logfile
-mas list       | sed 's/^/mas: /'  >> $logfile
+brew tap         | sed 's/^/tap: /'  >> $logfile
+brew leaves      | sed 's/^/brew: /' >> $logfile
+brew list --cask | sed 's/^/cask: /' >> $logfile
+mas list         | sed 's/^/mas: /'  >> $logfile
 echo 'done.'
