00f0f2bd2e224ccbf9123224f109e08434a4ab84
kawarimidoll <kawarimidoll+git@gmail.com>
2021-02-26 10:29:22 +0900

    add scripts for vscode

--- etc/mac/install.sh
+++ etc/mac/install.sh
@@ -8,6 +8,11 @@ karabiner_dir="$HOME/.config/karabiner"
 mkdir -p "$karabiner_dir"
 ln -sniv karabiner.json "$karabiner_dir/karabiner.json"
 
+echo "  enable key-repeating for VScodeVim."
+defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false         # For VS Code
+defaults write com.microsoft.VSCodeInsiders ApplePressAndHoldEnabled -bool false # For VS Code Insider
+defaults write com.visualstudio.code.oss ApplePressAndHoldEnabled -bool false    # For VS Codium
+
 echo ""
 echo "  Next step: Download Xcode from AppStore."
 echo "  After download, run commands below to setup applications."
