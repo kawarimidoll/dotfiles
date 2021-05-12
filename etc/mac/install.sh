de427219fea7a28a2463ef8cd61870c920bd1155
kawarimidoll <kawarimidoll+git@gmail.com>
2021-05-12 14:17:11 +0900

    add lazygit config

--- etc/mac/install.sh
+++ etc/mac/install.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-echo "  create symlink to iCloud directory."
-ln -sniv "$HOME/Library/Mobile\ Documents" "$HOME/iCloud"
+# echo "  create symlink to iCloud directory."
+# ln -sniv "$HOME/Library/Mobile\ Documents" "$HOME/iCloud"
 
-echo "  enable key-repeating for VScodeVim."
-defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false         # For VS Code
-defaults write com.microsoft.VSCodeInsiders ApplePressAndHoldEnabled -bool false # For VS Code Insider
-defaults write com.visualstudio.code.oss ApplePressAndHoldEnabled -bool false    # For VS Codium
+echo "  create symlink to lazygit config directory."
+mkdir -p "$HOME/Library/Application Support/jesseduffield/lazygit"
+ln -sniv "$HOME/.config/lazygit/config.yml" "$HOME/Library/Application Support/jesseduffield/lazygit/config.yml"
+
+# echo "  enable key-repeating for VScodeVim."
+# defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false         # For VS Code
+# defaults write com.microsoft.VSCodeInsiders ApplePressAndHoldEnabled -bool false # For VS Code Insider
+# defaults write com.visualstudio.code.oss ApplePressAndHoldEnabled -bool false    # For VS Codium
 
 echo ""
 echo "  Next step: Download Xcode from AppStore."
