b480498c9de127bd9c5f2be14ee41caa12a31d89
kawarimidoll <kawarimidoll+git@gmail.com>
2021-08-12 11:03:32 +0900

    update xbar-github-contributions

--- etc/mac/xbar-plugins/github-contributions.15m.sh
+++ etc/mac/xbar-plugins/github-contributions.15m.sh
@@ -7,25 +7,27 @@
 # <xbar.author.github>kawarimidoll</xbar.author.github>
 # <xbar.desc>Show github contributions of today.</xbar.desc>
 # <xbar.image></xbar.image>
-# <xbar.dependencies>curl</xbar.dependencies>
+# <xbar.dependencies>curl,jq(homebrew)</xbar.dependencies>
 
 # Variables:
 # <xbar.var>string(VAR_USERNAME="kawarimidoll"): Your github username.</xbar.var>
 
-curl -sS "https://github.com/users/${VAR_USERNAME}/contributions" | \
-  grep 'data-date' | \
-  tail -7 | \
-  sed -r 's/.+count="([0-9]+)".+date="([0-9\-]+)".+/\2: \1/' | \
-  awk '{
-    if ($2 == 0) print ":fallen_leaf:", $0, "| color=brown";
-    else if ($2 < 5) print ":seedling:", $0, "| color=green";
-    else if ($2 < 10) print ":herb:", $0, "| color=green";
-    else print ":evergreen_tree:", $0, "| color=green";
-    }' | \
+curl -sS https://github-contributions-api.deno.dev/${VAR_USERNAME}.json?flat=true | \
+  /opt/homebrew/bin/jq -r '.contributions[-7:] | map(
+      (
+        if .contributionLevel == "NONE" then ":fallen_leaf:"
+        elif .contributionLevel == "FIRST_QUARTILE" then ":seedling:"
+        elif .contributionLevel == "SECOND_QUARTILE" then ":harb:"
+        else ":evergreen_tree:" end
+      ) + .date + ": " +
+      (.contributionCount | tostring) + " | color=" +
+      (if .contributionLevel == "NONE" then "brown" else "green" end)
+    ) | .[]' | \
   sed '1!G;h;$!d' | \
   sed -e '1a\'$'\n''---'
 echo '---'
 echo "Open GitHub | href=https://github.com/${VAR_USERNAME}"
 
+# [jq not working · Issue #732 · matryer/xbar](https://github.com/matryer/xbar/issues/732)
 # tac with sed: https://stackoverflow.com/questions/742466/how-can-i-reverse-the-order-of-lines-in-a-file
 # insert on mac sed: https://unix.stackexchange.com/questions/52131/sed-on-osx-insert-at-a-certain-line
