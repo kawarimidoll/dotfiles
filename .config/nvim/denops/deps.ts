701d8302197e9059d62b6bf03072754cd7700b62
kawarimidoll <kawarimidoll+git@gmail.com>
2021-12-28 10:24:05 +0900

    refactor: improve denops settings

---
+++ .config/nvim/denops/deps.ts
@@ -0,0 +1,3 @@
+export type { Context, Denops } from "https://deno.land/x/denops_std@v2.2.0/mod.ts";
+export { execute } from "https://deno.land/x/denops_std@v2.2.0/helper/mod.ts";
+export { ensureString } from "https://deno.land/x/unknownutil@v1.1.4/mod.ts";
--- .config/nvim/denops/kwrm/main.ts
+++ .config/nvim/denops/kwrm/main.ts
@@ -1,8 +1,13 @@
-import { Denops } from "https://deno.land/x/denops_std@v2.2.0/mod.ts";
-import { execute } from "https://deno.land/x/denops_std@v2.2.0/helper/mod.ts";
-import { ensureString } from "https://deno.land/x/unknownutil@v1.1.4/mod.ts";
+import { Context, Denops, ensureString, execute } from "../deps.ts";
 
 export async function main(denops: Denops): Promise<void> {
+  function dxecute(script: string | string[], ctx?: Context) {
+    return execute(denops, script, ctx);
+  }
+  function dequest(name: string, arg: string) {
+    return `denops#request('${denops.name}', '${name}', [${arg}])`;
+  }
+
   denops.dispatcher = {
     async denopsEcho(text: unknown): Promise<unknown> {
       ensureString(text);
@@ -18,18 +23,17 @@ export async function main(denops: Denops): Promise<void> {
     },
   };
 
-  await execute(
-    denops,
-    `command! -nargs=1 DenopsEcho echomsg denops#request('${denops.name}', 'denopsEcho', [<q-args>])`,
+  await dxecute(
+    `command! -nargs=1 DenopsEcho echomsg ${dequest("denopsEcho", "<q-args>")}`,
   );
-  await execute(denops, [
+  await dxecute([
     `function! EncodeURI(arg)`,
-    `return denops#request('${denops.name}', 'denopsEncodeURI', [a:arg])`,
+    `return ${dequest("denopsEncodeURI", "a:arg")}`,
     `endfunction`,
   ]);
-  await execute(denops, [
+  await dxecute([
     `function! DecodeURI(arg)`,
-    `return denops#request('${denops.name}', 'denopsDecodeURI', [a:arg])`,
+    `return ${dequest("denopsDecodeURI", "a:arg")}`,
     `endfunction`,
   ]);
 }
