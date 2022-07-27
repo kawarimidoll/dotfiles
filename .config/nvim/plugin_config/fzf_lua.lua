9ad1683f83e893cb1363f4136fae61d1bcf4ad13
kawarimidoll <kawarimidoll+git@gmail.com>
2022-07-27 17:30:47 +0900

    feat: update fzf-lua settings

--- .config/nvim/plugin_config/fzf_lua.lua
+++ .config/nvim/plugin_config/fzf_lua.lua
@@ -1,4 +1,6 @@
-require('fzf-lua').setup({
+local fzf_lua = require('fzf-lua')
+
+fzf_lua.setup({
   winopts = {
     height = 0.90,
     width  = 0.90,
@@ -6,9 +8,9 @@ require('fzf-lua').setup({
     col    = 0.50,
   },
   keymap = {
-  -- These override the default tables completely
-  -- no need to set to `false` to disable a bind
-  -- delete or modify is sufficient
+    -- These override the default tables completely
+    -- no need to set to `false` to disable a bind
+    -- delete or modify is sufficient
     builtin = {
       ["<C-/>"]    = "toggle-help",
       ["<C-;>"]    = "toggle-preview-wrap",
@@ -22,6 +24,14 @@ require('fzf-lua').setup({
   },
   fzf_opts = {
     ['--cycle'] = '',
+  },
+  actions = {
+    files = {
+      ["default"] = fzf_lua.actions.file_edit,
+    },
+  },
+  files = {
+    cmd = 'find_for_vim',
   },
   grep = {
     rg_opts = "--column --line-number --no-heading --color=always --smart-case --max-columns=512 --hidden --trim",
