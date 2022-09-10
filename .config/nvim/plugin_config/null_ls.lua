01ac35bb1cd200be7104a805490a0f7383afbfbc
kawarimidoll <kawarimidoll+git@gmail.com>
2022-09-10 13:42:04 +0900

    feat: add null-ls completion

--- .config/nvim/plugin_config/null_ls.lua
+++ .config/nvim/plugin_config/null_ls.lua
@@ -27,6 +27,10 @@ table.insert(prettier_configs, 'prettier.config.js')
 table.insert(prettier_configs, 'prettier.config.cjs')
 
 local sources = {
+  -- completion
+  null_ls.builtins.completion.spell,
+  null_ls.builtins.completion.vsnip,
+
   -- diagnostics
   null_ls.builtins.diagnostics.cspell.with({
     diagnostics_postprocess = function(diagnostic)
@@ -127,6 +131,129 @@ local cspell_custom_actions = {
   }
 }
 null_ls.register(cspell_custom_actions)
+
+local get_candidates = function(entries, opts)
+  opts = opts or {}
+  local detail = opts.detail or ''
+  local kind = opts.kind or vim.lsp.protocol.CompletionItemKind['Text']
+  local items = {}
+  for _, v in ipairs(entries) do
+    if v ~= '' then
+      table.insert(items, { label = v, detail = detail, kind = kind })
+    end
+  end
+  return items
+end
+
+local rg_dictionary_completion = {
+  name = 'rg-dictionary',
+  meta = { description = "rg-dictionary completion source.", },
+  method = null_ls.methods.COMPLETION,
+  filetypes = {},
+  generator = {
+    fn = function(params, done)
+      local dictionaries = vim.api.nvim_get_option_value('dictionary', {})
+      local src = vim.fn.substitute(dictionaries, ',', ' ', 'g')
+      local pattern = '^' .. params.word_to_complete
+
+      -- local file = io.popen('look ' .. params.word_to_complete)
+      local cmd = table.concat({ 'rg',
+        '--ignore-case',
+        '--no-heading',
+        '--no-line-number',
+        '--color=never',
+        pattern,
+        src
+      }, ' ')
+      local file = io.popen(cmd)
+      local stdout = ''
+      if file then
+        stdout = file:read('a')
+        file:close()
+      end
+
+      local kind = vim.lsp.protocol.CompletionItemKind['Text']
+      local candidates = {}
+      for _, v in ipairs(vim.split(stdout, '\r?\n')) do
+        if v ~= '' then
+          local path_and_word = vim.split(v, ':')
+          local label = path_and_word[2]
+          local detail = string.gsub(path_and_word[1], '.*/', '[dic] ')
+
+          table.insert(candidates, { label = label, detail = detail, kind = kind })
+        end
+      end
+      done({ { items = candidates, isIncomplete = #candidates > 0 } })
+    end,
+    async = true,
+  },
+}
+null_ls.register(rg_dictionary_completion)
+
+local ex_commands_completion = {
+  name = 'ex-commands',
+  meta = { description = "ex-commands completion source.", },
+  method = null_ls.methods.COMPLETION,
+  filetypes = { 'vim', 'lua' },
+  generator = {
+    fn = function(params, done)
+      local cmds = vim.fn.getcompletion(params.word_to_complete, 'command', 1)
+
+      local candidates = get_candidates(cmds, { detail = '[cmd]' })
+      done({ { items = candidates, isIncomplete = #candidates > 0 } })
+    end,
+    async = true,
+  },
+}
+null_ls.register(ex_commands_completion)
+
+local file_completion = {
+  name = 'file',
+  meta = { description = "file completion source.", },
+  method = null_ls.methods.COMPLETION,
+  filetypes = {},
+  generator = {
+    fn = function(params, done)
+      local cmds = vim.fn.getcompletion(params.word_to_complete, 'file', 1)
+
+      local candidates = get_candidates(cmds, { detail = '[file]' })
+      done({ { items = candidates, isIncomplete = #candidates > 0 } })
+    end,
+    async = true,
+  },
+}
+null_ls.register(file_completion)
+
+local around_completion = {
+  name = 'around',
+  meta = { description = "around completion source.", },
+  method = null_ls.methods.COMPLETION,
+  filetypes = {},
+  generator = {
+    fn = function(params, done)
+      local term = params.word_to_complete
+      local range = 100
+      local current_line = vim.fn.line('.')
+      local lnum_from = math.max(1, current_line - range)
+      local lnum_to = math.min(vim.fn.line('$'), current_line + range)
+
+      local lines = vim.fn.getline(lnum_from, lnum_to)
+      local aggregated = {}
+      for _, x in ipairs(lines) do
+        for w in string.gmatch(x, '[_%w][_%w]+') do
+          if w ~= term and vim.startswith(w, term) then
+            aggregated[w] = 1
+          end
+        end
+      end
+
+      local candidates = get_candidates(vim.tbl_keys(aggregated), { detail = '[A]' })
+      done({ { items = candidates, isIncomplete = #candidates > 0 } })
+    end,
+    async = true,
+  },
+}
+null_ls.register(around_completion)
 
 null_ls.setup({
   sources = sources
