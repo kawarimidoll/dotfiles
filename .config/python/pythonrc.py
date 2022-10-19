91e6427191eb77f391483e6a018b5662a72fd587
kawarimidoll <kawarimidoll+git@gmail.com>
2022-10-19 12:01:48 +0900

    feat: add xdg settings

---
+++ .config/python/pythonrc.py
@@ -0,0 +1,19 @@
+import os
+import atexit
+import readline
+
+# Save history in XDG_CACHE_HOME/python_history
+# Note: This won't work if python is invoked with -i flag.
+history = os.path.join(os.environ['XDG_CACHE_HOME'], 'python_history')
+try:
+    readline.read_history_file(history)
+except OSError:
+    pass
+
+def write_history():
+    try:
+        readline.write_history_file(history)
+    except OSError:
+        pass
+
+atexit.register(write_history)
