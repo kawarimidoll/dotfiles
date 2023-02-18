5ab70f2ed3b38e1b5ba73705517f5024ebf7f4c1
kawarimidoll <kawarimidoll+git@gmail.com>
2023-02-18 17:01:51 +0900

    feat: use vtsls instead of tsserver

--- .config/nvim/plugin_config/lsp.lua
+++ .config/nvim/plugin_config/lsp.lua
@@ -22,7 +22,7 @@ local update_capabilities = function(capabilities)
   completionItem.commitCharactersSupport = true
   completionItem.tagSupport = { valueSet = { 1 } }
   completionItem.resolveSupport =
-    { properties = { 'documentation', 'detail', 'additionalTextEdits' } }
+  { properties = { 'documentation', 'detail', 'additionalTextEdits' } }
 
   return capabilities
 end
@@ -31,7 +31,7 @@ vim.keymap.set('n', '<space>p', function()
   if vim.lsp.buf.format then
     vim.lsp.buf.format({
       filter = function(client)
-        return client.name ~= 'tsserver' and client.name ~= 'jsonls'
+        return client.name ~= 'vtsls' and client.name ~= 'jsonls'
       end,
     })
   else
@@ -50,18 +50,22 @@ mason_lspconfig.setup_handlers({
     opts.capabilities = update_capabilities(vim.lsp.protocol.make_client_capabilities())
     -- vim.api.nvim_echo({{'server_name'}, {server_name, 'warningmsg'}}, true, {})
 
-    if server_name == 'tsserver' then
+    if server_name == 'vtsls' then
       if not is_node_repo then
         return
       end
-      opts.settings = { documentFormatting = false }
+      opts.settings = {
+        documentFormatting = false,
+        javascript = { suggest = { completeFunctionCalls = true } },
+        typescript = { suggest = { completeFunctionCalls = true } },
+      }
     elseif server_name == 'denols' then
       if is_node_repo then
         return
       end
 
       opts.root_dir =
-        nvim_lsp.util.root_pattern('deno.json', 'deno.jsonc', 'deps.ts', 'import_map.json')
+          nvim_lsp.util.root_pattern('deno.json', 'deno.jsonc', 'deps.ts', 'import_map.json')
       opts.init_options = {
         lint = true,
         unstable = true,
