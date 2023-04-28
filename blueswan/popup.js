72a2e3baa3a58607040aa7e95afce634924dece7
kawarimidoll <kawarimidoll+git@gmail.com>
2023-04-29 08:24:25 +0900

    feat: blueswan add classes to body when loaded

--- blueswan/popup.js
+++ blueswan/popup.js
@@ -2,40 +2,32 @@ const names = [
   "replys",
   "reposts",
   "likes",
-  "followings",
   "followers",
+  "following",
+  "posts",
 ];
 
 function ensureValue(value) {
   return ["show", "hover", "hide"].includes(value) ? value : "show";
 }
 
-console.log("ho");
 function load() {
-  console.log("hi");
-  chrome.storage.local.get("blueswan", (data) => {
-    console.log(data);
-    // const values = items.blueswan;
-    // names.forEach((name) => {
-    //   document.getElementByName(name).value = ensureValue(values[name]);
-    // });
+  chrome.storage.local.get(names, (data) => {
+    names.forEach((name) => {
+      const select = document.getElementById(`select-${name}`);
+      [...select.options].forEach((option) =>
+        option.selected = option.value === ensureValue(data[name])
+      );
+      select.addEventListener("change", (event) => {
+        // body.classList.add(`${ensureValue(data[name])}-${name}`);
+
+        chrome.storage.local.set(
+          { [name]: event.target.value },
+          function () {},
+        );
+      });
+    });
   });
 }
 
-
-function save() {
-  const values = {};
-  names.forEach((name) => {
-    values[name] = ensureValue(
-      [...document.getElementsByName(name)]?.find((element) => element.checked)
-        ?.value,
-    );
-  });
-
-  chrome.storage.local.set({ "blueswan": values }, function () { });
-
-  document.getElementById("save").innerText = "saved";
-}
-
-document.getElementById("save").addEventListener("click", save);
-load()
+load();
--- blueswan/script.js
+++ blueswan/script.js
@@ -1,8 +1,42 @@
+const visibilities = ["show", "hover", "hide"];
+const names = [
+  "replys",
+  "reposts",
+  "likes",
+  "followers",
+  "following",
+  "posts",
+];
+
 function ensureValue(value) {
-  return ["show", "hover", "hide"].includes(value) ? value : "show";
+  return visibilities.includes(value) ? value : "show";
 }
 
-chrome.storage.local.get("Blueswing", (items) => {
-  console.log(items, JSON.stringify(items));
-  alert('loaded')
-});
+function setClass(name, value) {
+  visibilities.forEach((visibilitiy) => {
+    document.body.classList.remove(`${name}-${visibilitiy}`);
+  });
+  document.body.classList.add(`${name}-${value}`);
+}
+
+function setClasses() {
+  chrome.storage.local.get(names, (data) => {
+    console.log(data);
+    names.forEach((name) => {
+      setClass(name, ensureValue(data[name]));
+    });
+  });
+}
+setClasses();
+
+// function setClasses() {
+//   chrome.storage.local.get(names, (data) => {
+//     console.log(data);
+//     names.forEach((name) => {
+//       visibilities.forEach((visibilitiy) => {
+//         document.body.classList.remove(`${visibilitiy}-${name}`);
+//       });
+//       document.body.classList.add(`${ensureValue(data[name])}-${name}`);
+//     });
+//   });
+// }
