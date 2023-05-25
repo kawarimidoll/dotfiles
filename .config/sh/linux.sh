badc28d61da116aee3ceddc6ec8b6c70c3d6964c
kawarimidoll <kawarimidoll+git@gmail.com>
2023-05-25 15:32:16 +0900

    feat: add alias for linux

--- .config/sh/linux.sh
+++ .config/sh/linux.sh
@@ -5,3 +5,5 @@
 # [ターミナルで標準出力をクリップボードにコピーする - Qiita](https://qiita.com/sasaplus1/items/137a70e8f51f97a6636f)
 alias pbcopy='xsel --clipboard --input'
 alias pbpaste='xsel --clipboard --output'
+
+alias arwer="yay -Qeq > ${DOT_OS_DIR}/yay-list.log"
