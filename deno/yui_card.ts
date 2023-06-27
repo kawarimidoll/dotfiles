2aa1d075061419dc19ffdc4308cc74904afbbb7a
kawarimidoll <kawarimidoll+git@gmail.com>
2023-06-28 08:19:57 +0900

    fix: update yui handle

--- deno/yui_card.ts
+++ deno/yui_card.ts
@@ -1,5 +1,5 @@
 import { richPost } from "./bsky_post.ts";
 
-await richPost("@yui.bsky.social card");
-await richPost("@yui.bsky.social card -b");
-await richPost("@yui.bsky.social card ai");
+await richPost("@yui.syui.ai card");
+await richPost("@yui.syui.ai card -b");
+await richPost("@yui.syui.ai card ai");
