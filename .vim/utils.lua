04bf141c95c499276781b8052725ee8997f9f079
kawarimidoll <kawarimidoll+git@gmail.com>
2024-01-20 17:24:45 +0900

    feat: add vim.lambda inspired by uga-rosa's article
    Co-Authored-By: uga-rosa <82267684+uga-rosa@users.noreply.github.com>

--- .vim/utils.lua
+++ .vim/utils.lua
@@ -16,3 +16,18 @@ if not vim.bool_fn then
     end,
   })
 end
+
+-- https://zenn.dev/uga_rosa/articles/2548e7517b8a8d
+-- example:
+-- local add = vim.lambda[[ x, y: x + y ]]
+-- print(add(1, 2))
+-- -> output: 3
+if not vim.lambda then
+  ---@param str string
+  ---@return function
+  vim.lambda = function(str)
+    local chunk = [[return function(%s) return %s end]]
+    local arg, body = str:match('(.*):(.*)')
+    return assert(load(chunk:format(arg, body)))()
+  end
+end
