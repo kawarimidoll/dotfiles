62d67bd0c216bf3f85c0ef3ae343641781a83ac6
kawarimidoll <kawarimidoll+git@gmail.com>
2024-03-09 21:29:37 +0900

    feat: improve mini.lua

--- .config/nvim/plugin_config/mini.lua
+++ .config/nvim/plugin_config/mini.lua
@@ -67,6 +67,81 @@ vim.api.nvim_create_autocmd({ 'VimEnter' }, {
   once = true,
 })
 
+vim.api.nvim_create_autocmd({ 'VimEnter' }, {
+  pattern = '*',
+  callback = function()
+    local hipatterns = require('mini.hipatterns')
+    hipatterns.setup({
+      highlighters = {
+        hex_color = hipatterns.gen_highlighter.hex_color(),
+      },
+    })
+  end,
+  once = true,
+})
+
+vim.api.nvim_create_autocmd({ 'VimEnter' }, {
+  pattern = '*',
+  callback = function()
+    local miniclue = require('mini.clue')
+    miniclue.setup({
+      triggers = {
+        -- Leader triggers
+        { mode = 'n', keys = '<Leader>' },
+        { mode = 'x', keys = '<Leader>' },
+
+        -- Built-in completion
+        { mode = 'i', keys = '<C-x>' },
+
+        -- `g` key
+        { mode = 'n', keys = 'g' },
+        { mode = 'x', keys = 'g' },
+
+        -- Marks
+        { mode = 'n', keys = "'" },
+        { mode = 'n', keys = '`' },
+        { mode = 'x', keys = "'" },
+        { mode = 'x', keys = '`' },
+
+        -- Registers
+        { mode = 'n', keys = '"' },
+        { mode = 'x', keys = '"' },
+        { mode = 'i', keys = '<C-r>' },
+        { mode = 'c', keys = '<C-r>' },
+
+        -- Window commands
+        { mode = 'n', keys = '<C-w>' },
+
+        -- `z` key
+        { mode = 'n', keys = 'z' },
+        { mode = 'x', keys = 'z' },
+      },
+
+      clues = {
+        -- Enhance this by adding descriptions for <Leader> mapping groups
+        miniclue.gen_clues.builtin_completion(),
+        miniclue.gen_clues.g(),
+        miniclue.gen_clues.marks(),
+        miniclue.gen_clues.registers(),
+        miniclue.gen_clues.windows(),
+        miniclue.gen_clues.z(),
+      },
+    })
+  end,
+  once = true,
+})
+
+vim.api.nvim_create_autocmd({ 'VimEnter' }, {
+  pattern = '*',
+  callback = function()
+    require('mini.notify').setup()
+
+    local opts = { ERROR = { duration = 10000 } }
+    vim.notify = require('mini.notify').make_notify(opts)
+  end,
+  once = true,
+})
+
 -- vim.api.nvim_create_autocmd({ 'VimEnter' }, {
 --   pattern = '*',
 --   callback = function()
