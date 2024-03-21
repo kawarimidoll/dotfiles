1e380646969940855cee461422fc48d35af04907
kawarimidoll <kawarimidoll+git@gmail.com>
2024-03-22 08:33:27 +0900

    feat: add base16_palettes.lua

---
+++ .config/nvim/base16_palettes.lua
@@ -0,0 +1,34 @@
+-- Default Background
+-- Lighter Background (Used for status bars, line number and folding marks)
+-- Selection Background
+-- Comments, Invisible, Line Highlighting
+-- Dark Foreground (Used for status bars)
+-- Default Foreground, Caret, Delimiters, Operators
+-- Light Foreground (Not often used)
+-- Light Background (Not often used)
+-- Variables, XML Tags, Markup Link Text, Markup Lists, Diff Deleted
+-- Integers, Boolean, Constants, XML Attributes, Markup Link Url
+-- Classes, Markup Bold, Search Text Background
+-- Strings, Inherited Class, Markup Code, Diff Inserted
+-- Support, Regular Expressions, Escape Characters, Markup Quotes
+-- Functions, Methods, Attribute IDs, Headings
+-- Keywords, Storage, Selector, Markup Italic, Diff Changed
+-- Deprecated, Opening/Closing Embedded Language Tags, e.g. <?php ?>
+return {
+  -- https://github.com/sainnhe/sonokai/blob/master/alacritty/README.md shusia
+  shusia = { '#2d2a2e', '#37343a', '#423f46', '#848089', '#66d9ef', '#e3e1e4', '#a1efe4', '#f8f8f2', '#f85e84', '#ef9062', '#a6e22e', '#e5c463', '#66d9ef', '#9ecd6f', '#a1efe4', '#f9f8f5', },
+  -- https://github.com/KeitaNakamura/neodark.vim/blob/master/colors/neodark.vim
+  neodark = { '#1F2F38', '#263A45', '#475C69', '#658595', '#D4AE58', '#AABBC4', '#C99720', '#72C7D1', '#4BB1A7', '#DC657D', '#B888E2', '#E18254', '#E69CA0', '#639EE4', '#84B97C', '#AE8785', },
+  -- https://base16-project.github.io/base16-gallery/
+  kanagawa = { "#1F1F28", "#16161D", "#223249", "#54546D", "#727169", "#DCD7BA", "#C8C093", "#717C7C", "#C34043", "#FFA066", "#C0A36E", "#76946A", "#6A9589", "#7E9CD8", "#957FB8", "#D27E99", },
+  materia = { '#263238', '#2C393F', '#37474F', '#707880', '#C9CCD3', '#CDD3DE', '#D5DBE5', '#FFFFFF', '#EC5F67', '#EA9560', '#FFCC00', '#8BD649', '#80CBC4', '#89DDFF', '#82AAFF', '#EC5F67', },
+  everforest = { '#2b3339', '#323c41', '#3a4248', '#868d80', '#a59572', '#d3c6aa', '#e9e8d2', '#fff9e8', '#7fbbb3', '#d699b6', '#83c092', '#dbbc7f', '#e69875', '#a7c080', '#e67e80', '#d699b6', },
+  atlas = { "#002635", "#00384d", "#517F8D", "#6C8B91", "#869696", "#a1a19a", "#e6e6dc", "#fafaf8", "#ff5a67", "#f08e48", "#ffcc1b", "#7fc06e", "#5dd7b9", "#14747e", "#9a70a4", "#c43060", },
+  ayu_mirage = { '#171B24', '#1F2430', '#242936', '#707A8C', '#8A9199', '#CCCAC2', '#D9D7CE', '#F3F4F5', '#F28779', '#FFAD66', '#FFD173', '#D5FF80', '#95E6CB', '#5CCFE6', '#D4BFFF', '#F29E74', },
+  darcula = { "#2b2b2b", "#323232", "#323232", "#606366", "#a4a3a3", "#a9b7c6", "#ffc66d", "#ffffff", "#4eade5", "#689757", "#bbb529", "#6a8759", "#629755", "#9876aa", "#cc7832", "#808080", },
+  atelier_dune = { "#20201d", "#292824", "#6e6b5e", "#7d7a68", "#999580", "#a6a28c", "#e8e4cf", "#fefbec", "#d73737", "#b65611", "#ae9513", "#60ac39", "#1fad83", "#6684e1", "#b854d4", "#d43552", },
+  catppuccin = { '#1E1E28', '#1A1826', '#302D41', '#575268', '#6E6C7C', '#D7DAE0', '#F5E0DC', '#C9CBFF', '#F28FAD', '#F8BD96', '#FAE3B0', '#ABE9B3', '#B5E8E0', '#96CDFB', '#DDB6F2', '#F2CDCD', },
+  eighties = { '#2d2d2d', '#393939', '#515151', '#747369', '#a09f93', '#d3d0c8', '#e8e6df', '#f2f0ec', '#f2777a', '#f99157', '#ffcc66', '#99cc99', '#66cccc', '#6699cc', '#cc99cc', '#d27b53', },
+  tokyo_city_terminal_dark = { '#171D23', '#1D252C', '#28323A', '#526270', '#B7C5D3', '#D8E2EC', '#F6F6F8', '#FBFBFD', '#D95468', '#FF9E64', '#EBBF83', '#8BD49C', '#70E1E8', '#539AFC', '#B62D65', '#DD9D82', },
+  woodland = { '#231e18', '#302b25', '#48413a', '#9d8b70', '#b4a490', '#cabcb1', '#d7c8bc', '#e4d4c8', '#d35c5c', '#ca7f32', '#e0ac16', '#b7ba53', '#6eb958', '#88a4d3', '#bb90e2', '#b49368', },
+}
