5398913ee7ed38f1f97a3cadb2e7d39f0228f61a
kawarimidoll <kawarimidoll+git@gmail.com>
2024-04-01 22:23:50 +0900

    feat: improve mini.statusline using mini.diff

--- .config/nvim/minideps.lua
+++ .config/nvim/minideps.lua
@@ -40,11 +40,61 @@ now(function()
     pattern = '*',
     callback = require('nvim-web-devicons').setup
   })
-
 end)
 
 now(function()
-  require('mini.statusline').setup({ set_vim_settings = false })
+  local diff_summary = function()
+    local summary = vim.b.minidiff_summary or {}
+    if type(summary) ~= 'table' or vim.tbl_isempty(summary) then
+      return ''
+    end
+    local status_txt = {}
+    if summary.add and summary.add > 0 then
+      table.insert(status_txt, '+' .. summary.add)
+    end
+    if summary.change and summary.change > 0 then
+      table.insert(status_txt, '~' .. summary.change)
+    end
+    if summary.remove and summary.remove > 0 then
+      table.insert(status_txt, '-' .. summary.remove)
+    end
+    return table.concat(status_txt, ' ')
+  end
+  local section_diff = function(args)
+    if vim.bo.buftype ~= '' then return '' end
+
+    local signs = MiniStatusline.is_truncated(args.trunc_width) and '' or (diff_summary() or '')
+    local icon = ''
+
+    return string.format('%s %s', icon, signs)
+  end
+  require('mini.statusline').setup({
+    content = {
+      active = function()
+        local mode, mode_hl = MiniStatusline.section_mode({ trunc_width = 120 })
+        local git = section_diff({ trunc_width = 75 })
+        local diagnostics = MiniStatusline.section_diagnostics({ trunc_width = 75 })
+        local filename = MiniStatusline.section_filename({ trunc_width = 140 })
+        local fileinfo = MiniStatusline.section_fileinfo({ trunc_width = 120 })
+        local location = MiniStatusline.section_location({ trunc_width = 75 })
+        local searchcount = MiniStatusline.section_searchcount({ trunc_width = 75 })
+        if vim.fn.empty(searchcount) == 0 then
+          searchcount = '[' .. searchcount .. ']'
+        end
+
+        return MiniStatusline.combine_groups({
+          { hl = mode_hl,                 strings = { mode } },
+          { hl = 'MiniStatuslineDevinfo', strings = { git, diagnostics } },
+          '%<', -- Mark general truncate point
+          { hl = 'MiniStatuslineFilename', strings = { filename } },
+          '%=', -- End left alignment
+          { hl = 'MiniStatuslineFileinfo', strings = { fileinfo } },
+          { hl = mode_hl,                  strings = { location, searchcount } },
+        })
+      end,
+    },
+    set_vim_settings = false,
+  })
   vim.opt.laststatus = 3
 end)
 
