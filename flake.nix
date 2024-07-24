88f7fefc9bcd07a0f6b8574a00c88f9462f0e2ad
kawarimidoll <kawarimidoll+git@gmail.com>
2024-07-24 14:46:35 +1000

    feat: add alejandra and neovim-nightly-overlay

--- flake.nix
+++ flake.nix
@@ -1,9 +1,16 @@
 {
   description = "Minimal package definition for aarch64-darwin";
 
-  inputs.nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
+  };
 
-  outputs = { self, nixpkgs }: {
+  outputs = {
+    self,
+    nixpkgs,
+    neovim-nightly-overlay,
+  }: {
     # install:
     #   nix profile install .#my-packages
     # uninstall:
@@ -13,13 +20,16 @@
     #   nix profile upgrade my-packages
     packages.aarch64-darwin.my-packages = nixpkgs.legacyPackages.aarch64-darwin.buildEnv {
       name = "my-packages-list";
-      paths = with nixpkgs.legacyPackages.aarch64-darwin; [
-        git
-        curl
-        jq
-        ripgrep
-        eza
-      ];
+      paths = with nixpkgs.legacyPackages.aarch64-darwin;
+        [
+          git
+          curl
+          jq
+          ripgrep
+          eza
+          alejandra
+        ]
+        ++ [neovim-nightly-overlay.packages.aarch64-darwin.neovim];
     };
   };
 }
