c604b9335962db08f7a588632cd4ecae5a687db4
kawarimidoll <kawarimidoll+git@gmail.com>
2024-07-27 22:55:54 +1000

    feat: add home-manager packages

--- .config/home-manager/home.nix
+++ .config/home-manager/home.nix
@@ -4,19 +4,26 @@
   config,
   pkgs,
   ...
-}: {
+}: let
+  username = "kawarimidoll";
+in {
   nixpkgs = {
     config = {
       allowUnfree = true;
     };
   };
 
-  home = rec {
-    username = "kawarimidoll";
+  home = {
+    username = username;
     homeDirectory = "/Users/${username}";
 
     # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
     stateVersion = "24.05";
+
+    packages = with pkgs; [
+      bat
+      bottom
+    ];
   };
 
   programs.home-manager.enable = true;
