2bd885c3ea484088cfbf9af1453eeac4ca80b3a7
kawarimidoll <kawarimidoll+git@gmail.com>
2024-08-03 21:39:58 +1000

    chore: simplify mini.statusline settings

--- .config/nvim/minideps.lua
+++ .config/nvim/minideps.lua
@@ -43,34 +43,7 @@ now(function()
 end)
 
 now(function()
-  require('mini.statusline').setup({
-    content = {
-      active = function()
-        local mode, mode_hl = MiniStatusline.section_mode({ trunc_width = 120 })
-        local git = MiniStatusline.section_git({ trunc_width = 40 })
-        local diff = MiniStatusline.section_diff({ trunc_width = 75 })
-        local diagnostics = MiniStatusline.section_diagnostics({ trunc_width = 75 })
-        local filename = MiniStatusline.section_filename({ trunc_width = 140 })
-        local fileinfo = MiniStatusline.section_fileinfo({ trunc_width = 120 })
-        local location = MiniStatusline.section_location({ trunc_width = 75 })
-        local searchcount = MiniStatusline.section_searchcount({ trunc_width = 75 })
-        if vim.fn.empty(searchcount) == 0 then
-          searchcount = '[' .. searchcount .. ']'
-        end
-
-        return MiniStatusline.combine_groups({
-          { hl = mode_hl,                 strings = { mode } },
-          { hl = 'MiniStatuslineDevinfo', strings = { git, diagnostics } },
-          '%<', -- Mark general truncate point
-          { hl = 'MiniStatuslineFilename', strings = { filename } },
-          '%=', -- End left alignment
-          { hl = 'MiniStatuslineFileinfo', strings = { fileinfo } },
-          { hl = mode_hl,                  strings = { location, searchcount } },
-        })
-      end,
-    },
-    set_vim_settings = false,
-  })
+  require('mini.statusline').setup({ set_vim_settings = false })
   vim.opt.laststatus = 3
 end)
 
