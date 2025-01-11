064cac0d052c7877feff6879d0ec911e5fb2fb72
kawarimidoll <kawarimidoll+git@gmail.com>
2025-01-11 21:51:31 +0900

    feat: add init.el

---
+++ .emacs.d/init.el
@@ -0,0 +1,11 @@
+;; 行番号の表示
+(global-display-line-numbers-mode 1)
+
+;; バックアップファイルを作らない
+(setq make-backup-files nil)
+
+;; 起動時にホームディレクトリに移動する
+(cd "~")
+
+;; スタートアップメッセージを非表示にする
+(setq inhibit-startup-message t)
