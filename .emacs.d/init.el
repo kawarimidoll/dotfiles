48cbb95b508784fc5d26e17538ff9d6c94f3d825
kawarimidoll <kawarimidoll+git@gmail.com>
2025-01-23 16:55:41 +0900

    feat: add emacs config

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,3 +1,4 @@
+;;; ChatGPTによる生成
 ;; 行番号の表示
 (global-display-line-numbers-mode 1)
 
@@ -9,3 +10,16 @@
 
 ;; スタートアップメッセージを非表示にする
 (setq inhibit-startup-message t)
+
+;;; Emacs実践入門
+;; c-mにnewline-and-indentを割り当てる（初期値はnewline）
+(global-set-key (kbd "C-m") 'newline-and-indent)
+
+;; c-hをバックスペースに
+(global-set-key (kbd "C-h") 'delete-backward-char)
+
+;; 折り返しトグル
+;(global-set-key (kbd "C-c l") 'toggle-truncate-lines)
+
+;; c-tをウィンドウ移動に
+(global-set-key (kbd "C-t") 'other-window)
