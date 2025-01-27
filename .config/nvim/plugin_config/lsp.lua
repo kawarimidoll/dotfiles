cf1ec0ff70a130095a491cd046666111a1ca4b09
kawarimidoll <kawarimidoll+git@gmail.com>
2025-01-27 17:55:16 +0900

    feat: add superhtml setting

--- .config/nvim/plugin_config/lsp.lua
+++ .config/nvim/plugin_config/lsp.lua
@@ -119,3 +119,15 @@ rust_opts.settings = {
   },
 }
 nvim_lsp.rust_analyzer.setup(rust_opts)
+
+-- superhtml
+vim.api.nvim_create_autocmd("Filetype", {
+  pattern = { "html", "shtml", "htm" },
+  callback = function()
+    vim.lsp.start({
+      name = "superhtml",
+      cmd = { "superhtml", "lsp" },
+      root_dir = vim.fs.dirname(vim.fs.find({ ".git" }, { upward = true })[1])
+    })
+  end
+})
