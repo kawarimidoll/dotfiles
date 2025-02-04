ee9a4fff975d2e0d742e0a541ae02360066eb485
kawarimidoll <kawarimidoll+git@gmail.com>
2025-02-04 23:04:06 +0900

    feat: add mini.git settings

--- .config/nvim/minideps.lua
+++ .config/nvim/minideps.lua
@@ -35,6 +35,29 @@ end)
 
 now(function()
   require('mini.git').setup()
+
+  local align_blame = function(au_data)
+    if au_data.data.git_subcommand ~= 'blame' then return end
+
+    -- Align blame output with source
+    local win_src = au_data.data.win_source
+    vim.wo.wrap = false
+    vim.fn.winrestview({ topline = vim.fn.line('w0', win_src) })
+    vim.api.nvim_win_set_cursor(0, { vim.fn.line('.', win_src), 0 })
+
+    -- Bind both windows so that they scroll together
+    vim.wo[win_src].scrollbind, vim.wo.scrollbind = true, true
+  end
+
+  local au_opts = { pattern = 'MiniGitCommandSplit', callback = align_blame }
+  vim.api.nvim_create_autocmd('User', au_opts)
+
+  vim.keymap.set(
+    { 'n', 'x' },
+    '<Leader>gs',
+    '<Cmd>lua MiniGit.show_at_cursor()<CR>',
+    { desc = 'Show at cursor' }
+  )
 end)
 
 now(function()
