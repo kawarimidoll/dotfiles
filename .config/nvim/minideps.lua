ddb4aa683521530ab4b18128ae56cb01451ab7e5
kawarimidoll <kawarimidoll+git@gmail.com>
2025-02-06 22:32:59 +0900

    feat: setup mini.hipatterns

--- .config/nvim/minideps.lua
+++ .config/nvim/minideps.lua
@@ -280,8 +280,15 @@ end)
 
 later(function()
   local hipatterns = require('mini.hipatterns')
+  local hi_words = require('mini.extra').gen_highlighter.words
   hipatterns.setup({
     highlighters = {
+      -- Highlight standalone 'FIXME', 'HACK', 'TODO', 'NOTE'
+      fixme = hi_words({ 'FIXME', 'Fixme', 'fixme' }, 'MiniHipatternsFixme'),
+      hack = hi_words({ 'HACK', 'Hack', 'hack' }, 'MiniHipatternsHack'),
+      todo = hi_words({ 'TODO', 'Todo', 'todo' }, 'MiniHipatternsTodo'),
+      note = hi_words({ 'NOTE', 'Note', 'note' }, 'MiniHipatternsNote'),
+      -- Highlight hex color strings (`#rrggbb`) using that color
       hex_color = hipatterns.gen_highlighter.hex_color(),
     },
   })
