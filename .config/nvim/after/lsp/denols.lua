f307dec3c17efad99c62271f9c7a3e08f05b299f
kawarimidoll <kawarimidoll+git@gmail.com>
2025-08-17 13:07:30 +0900

    feat: improve root_dir detection for denols

--- .config/nvim/after/lsp/denols.lua
+++ .config/nvim/after/lsp/denols.lua
@@ -1,15 +1,26 @@
 return {
   root_dir = function(bufnr, callback)
-    local found_dirs = vim.fs.find({
+    local cwd = vim.fs.dirname(vim.fs.normalize(vim.api.nvim_buf_get_name(bufnr)))
+    local deno_dirs = vim.fs.find({
       'deno.json',
       'deno.jsonc',
       'deps.ts',
     }, {
       upward = true,
-      path = vim.fs.dirname(vim.fs.normalize(vim.api.nvim_buf_get_name(bufnr))),
+      path = cwd,
     })
-    if #found_dirs > 0 then
-      return callback(vim.fs.dirname(found_dirs[1]))
+    if #deno_dirs > 0 then
+      return callback(vim.fs.dirname(deno_dirs[1]))
+    end
+
+    local node_dirs = vim.fs.find({
+      'package.json',
+    }, {
+      upward = true,
+      path = cwd,
+    })
+    if #node_dirs == 0 then
+      return callback(cwd)
     end
   end,
 }
