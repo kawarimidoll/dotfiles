a6f4d1d13a37b6d8d5e124c12e60df4b89b0decd
kawarimidoll <kawarimidoll+git@gmail.com>
2025-08-28 14:30:50 +0900

    refactor: simplify root_dir detection in denols and vtsls

--- .config/nvim/after/lsp/denols.lua
+++ .config/nvim/after/lsp/denols.lua
@@ -1,26 +1,19 @@
 return {
   root_dir = function(bufnr, callback)
-    local cwd = vim.fs.dirname(vim.fs.normalize(vim.api.nvim_buf_get_name(bufnr)))
-    local deno_dirs = vim.fs.find({
-      'deno.json',
-      'deno.jsonc',
-      'deps.ts',
-    }, {
-      upward = true,
-      path = cwd,
-    })
-    if #deno_dirs > 0 then
-      return callback(vim.fs.dirname(deno_dirs[1]))
+    local deno_markers = { 'deno.json', 'deno.jsonc', 'deps.ts' }
+    local deno_dir = vim.fs.root(bufnr, deno_markers)
+    if deno_dir then
+      return callback(deno_dir)
     end
 
-    local node_dirs = vim.fs.find({
-      'package.json',
-    }, {
-      upward = true,
-      path = cwd,
-    })
-    if #node_dirs == 0 then
-      return callback(cwd)
+    local node_markers =
+      { 'package-lock.json', 'yarn.lock', 'pnpm-lock.yaml', 'bun.lockb', 'bun.lock' }
+    local node_dir = vim.fs.root(bufnr, node_markers)
+    if node_dir then
+      return
     end
+
+    local cwd = vim.fs.dirname(vim.fs.normalize(vim.api.nvim_buf_get_name(bufnr)))
+    return callback(cwd)
   end,
 }
--- .config/nvim/after/lsp/vtsls.lua
+++ .config/nvim/after/lsp/vtsls.lua
@@ -17,15 +17,4 @@ return {
       preferGoToSourceDefinition = true,
     },
   },
-  root_dir = function(bufnr, callback)
-    local found_dirs = vim.fs.find({
-      'package.json',
-    }, {
-      upward = true,
-      path = vim.fs.dirname(vim.fs.normalize(vim.api.nvim_buf_get_name(bufnr))),
-    })
-    if #found_dirs > 0 then
-      return callback(vim.fs.dirname(found_dirs[1]))
-    end
-  end,
 }
