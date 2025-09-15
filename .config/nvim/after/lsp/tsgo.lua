7f291e9bd5e743212b3ee4d55d23335dd3734ec3
kawarimidoll <kawarimidoll+git@gmail.com>
2025-09-16 08:48:02 +0900

    fix: avoid to start tsgo in non-node project

--- .config/nvim/after/lsp/tsgo.lua
+++ .config/nvim/after/lsp/tsgo.lua
@@ -8,4 +8,12 @@ return {
       preferGoToSourceDefinition = true,
     },
   },
+  root_dir = function(bufnr, callback)
+    local node_markers =
+      { 'package-lock.json', 'yarn.lock', 'pnpm-lock.yaml', 'bun.lockb', 'bun.lock' }
+    local node_dir = vim.fs.root(bufnr, node_markers)
+    if node_dir then
+      callback(node_dir)
+    end
+  end,
 }
