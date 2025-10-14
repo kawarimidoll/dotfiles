f4b859453cdb6864710eb38555343dc4195a3422
kawarimidoll <kawarimidoll+git@gmail.com>
2025-10-14 10:27:53 +0900

    feat: use extui without pcall

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1294,18 +1294,15 @@ later(function()
 end)
 
 now(function()
-  local ok, extui = pcall(require, 'vim._extui')
-  if ok then
-    extui.enable({
-      enable = true, -- Whether to enable or disable the UI.
-      msg = { -- Options related to the message module.
-        ---@type 'cmd'|'msg' Where to place regular messages, either in the
-        ---cmdline or in a separate ephemeral message window.
-        target = 'msg',
-        timeout = 4000, -- Time a message is visible in the message window.
-      },
-    })
-  end
+  require('vim._extui').enable({
+    enable = true, -- Whether to enable or disable the UI.
+    msg = { -- Options related to the message module.
+      ---@type 'cmd'|'msg' Where to place regular messages, either in the
+      ---cmdline or in a separate ephemeral message window.
+      target = 'msg',
+      timeout = 4000, -- Time a message is visible in the message window.
+    },
+  })
 end)
 
 now(function()
