95f768c9bca782051959f4f35826ffeade16a978
kawarimidoll <kawarimidoll+git@gmail.com>
2025-11-18 18:28:24 +0900

    fix: workaround home-manager direnv issue with fish shell

--- nix/home-manager/programs/direnv.nix
+++ nix/home-manager/programs/direnv.nix
@@ -1,4 +1,5 @@
 # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.direnv.enable
+{ pkgs, ... }:
 {
   programs.direnv = {
     enable = true;
@@ -8,5 +9,16 @@
       warn_timeout = 0;
     };
     nix-direnv.enable = true;
+
+    # workaround
+    # https://github.com/nix-community/home-manager/issues/8170#issuecomment-3542413378
+    package = pkgs.direnv.overrideAttrs (oldAttrs: {
+      nativeCheckInputs = builtins.filter (pkg: pkg != pkgs.fish) (oldAttrs.nativeCheckInputs);
+      checkPhase = ''
+        runHook preCheck
+        make test-go test-bash test-zsh
+        runHook postCheck
+      '';
+    });
   };
 }
