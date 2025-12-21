43252e00f244c4cd08d6359d0583da9c1a94e61e
kawarimidoll <kawarimidoll+git@gmail.com>
2025-12-21 15:04:33 +0900

    refactor(mac): streamline brewer script
    - Removing the subshell and always use -ex
    - Replace the previous log file with Brewfile

--- etc/mac/brewer.sh
+++ etc/mac/brewer.sh
@@ -1,25 +1,19 @@
 #!/bin/bash
 # homebrew update script
 
-set -e
+set -ex
 
 date -Iseconds
 
-(
-  set -x
-  brew doctor
-  brew update
-  brew upgrade --fetch-HEAD
-  brew upgrade --cask
-  brew cleanup -s
-)
+brew doctor
+brew update
+brew upgrade --fetch-HEAD
+brew upgrade --cask
+brew cleanup -s
 
-echo 'log list...'
 {
-  date "+timestamp: %F %T %Z"
-  brew tap         | sed 's/^/tap: /'
-  brew leaves      | sed 's/^/brew: /'
-  brew list --cask | sed 's/^/cask: /'
-  mas-list | sort             | sed 's/^/mas: /'
-} > "$(dirname "$0")/brew-list.log"
+  date "+# timestamp: %F %T %Z"
+  brew bundle dump --no-vscode --no-go --no-flatpak --file=-
+} > "$(dirname "$0")/Brewfile"
+wc -l "$(dirname "$0")/Brewfile"
 echo 'done.'
