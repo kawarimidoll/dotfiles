eddd4dbfbff1d28e84e43e558f659d716a6d287b
kawarimidoll <kawarimidoll+git@gmail.com>
2026-01-24 22:48:54 +0900

    fix(lsp): use `oxlint --lsp` instead of oxc_language_server

--- .config/nvim/after/lsp/oxlint.lua
+++ .config/nvim/after/lsp/oxlint.lua
@@ -3,11 +3,11 @@ local get_node_root = require('mi.utils').get_node_root
 return {
   cmd = function(dispatchers, config)
     local cmd = (config or {}).root_dir
-      and config.root_dir .. '/node_modules/.bin/oxc_language_server'
+      and config.root_dir .. '/node_modules/.bin/oxlint'
     -- 本来ならexecutable()で確認するべきだが、node_modules/.binに入っている時点で
     -- 実行可能とみなして厳密なチェックは省略
     if vim.uv.fs_stat(cmd) then
-      return vim.lsp.rpc.start({ cmd }, dispatchers)
+      return vim.lsp.rpc.start({ cmd, '--lsp' }, dispatchers)
     end
   end,
   root_dir = function(bufnr, callback)
